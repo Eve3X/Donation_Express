478e2290ee69315f0e7a2e833028ee0cc957304a
Aditya Bhagat <122894830+Eve3X@users.noreply.github.com>
2024-02-19 14:47:26 +0530

    19/02

--- components/AboutArea.js
+++ components/AboutArea.js
@@ -9,7 +9,7 @@ const AboutArea = () => {
                         <div className="about-large-img">
                             <img src="/images/about-img.jpg" alt="" />
                         </div>
-                    </div> */}
+                    </div> */}  
                     <div className="col-lg-5">
                         <div className="about-semi-img">
                             <img src="/images/aboutus.png" alt="" />
@@ -24,7 +24,7 @@ const AboutArea = () => {
                                 <h2 className="section__title">Discover Donation Express</h2>
                                 <p className="section__meta" >learn about non profit agency</p>
                                 <p className="section__desc">
-                                Welcome to Donation Express, where the spirit of giving meets the drive for positive change. As an organization committed to making a difference, Donation Express focuses on community development, education, healthcare, and social welfare. With an inclusive approach, we embrace diversity and welcome individuals, volunteers, and organizations to join us on our mission. Transparency and accountability are our guiding principles, ensuring that every contribution counts toward creating a more equitable and sustainable world. At Donation Express, we believe in the power of collective action and invite you to be a part of our journey to uplift lives and build a brighter future for all.
+                                Welcome to Donation Express, where the spirit of giving meets the drive for positive change. As an organization committed to making a difference, Donation Express focuses on community development, education, healthcare, and social welfare. With an inclusive approach, we embrace diversity and welcome individuals, volunteers, and organizations to join us on our mission. Transparency and accountability are our guiding principles, ensuring that every contribution counts toward creating a more equitable and sustainable world. 
                                 </p>
                             </div>
                         </div>
--- components/MixerAreaTwo.js
+++ components/MixerAreaTwo.js
@@ -11,8 +11,8 @@ const MixerAreaTwo = () => {
                             <div className="section-icon">
                                 <img src="/images/section-icon.png" alt="section-icon" />
                             </div>
-                            <h2 className="section__title text__white">Our fingerprints on the lives we touch never
-                                fade</h2>
+                            <h2 className="section__title text__white">United, your generosity creates impactful change.
+                                </h2> 
                             <Link href="/donate" className="theme-btn">start donation</Link>
                         </div>
                     </div>
